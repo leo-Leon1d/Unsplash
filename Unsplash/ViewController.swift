9dfa9653fbc9099a075442859b7a120d6cba1a1c
Israelyan Leonid <leo.israelyan@icloud.com>
2022-05-15 12:48:04 +0300

    Initial Commit

---
+++ Unsplash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unsplash
+//
+//  Created by Леонид Исраелян on 15.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
